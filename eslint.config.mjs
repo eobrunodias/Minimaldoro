e8029da5a4d6de3ae0813bac63ad1d5f61bec9cd
Bruno Dias <brunodias40@gmail.com>
2024-08-05 12:58:18 -0300

    chore: update config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
+  { ignorePatterns: ["node_modules/", "dist/", "tailwind.config"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0C0910",
+        secondary: "#63A375",
+        textColor: "#faf6eb",
+      },
+      fontFamily: {
+        fontInter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
