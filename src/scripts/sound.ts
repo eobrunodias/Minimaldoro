930905da264f15b13291021ff23dcef9c3dd33d9
Bruno Dias <brunodias40@gmail.com>
2024-08-07 18:17:53 -0300

    feat: finish implement logic sound

--- src/scripts/sound.ts
+++ src/scripts/sound.ts
@@ -6,38 +6,68 @@ import {
   soundOn,
 } from "./elements";
 
-if (breakTag?.classList.contains("hidden")) {
-  if (backgroundMusic) {
-    backgroundMusic.volume = 0.1;
-    backgroundMusic.play();
-  }
-  if (breakMusic) {
-    breakMusic.pause();
-  }
-} else {
-  if (breakMusic) {
-    breakMusic.volume = 0.1;
-    breakMusic.play();
-  }
-  if (backgroundMusic) {
-    backgroundMusic.pause();
-  }
+let isSoundOn = false;
+
+if (backgroundMusic) {
+  backgroundMusic.volume = 0.1;
+  backgroundMusic.pause();
+}
+
+if (breakMusic) {
+  breakMusic.volume = 0.1;
+  breakMusic.pause();
 }
 
 soundOff?.addEventListener("click", () => {
   soundOff?.classList.add("hide");
   soundOn?.classList.remove("hide");
+  isSoundOn = true;
 
-  if (backgroundMusic) {
-    backgroundMusic.play();
+  if (breakTag?.classList.contains("hidden")) {
+    console.log("Playing background music");
+    backgroundMusic?.play();
+    breakMusic?.pause();
+  } else {
+    console.log("Playing break music");
+    breakMusic?.play();
+    backgroundMusic?.pause();
   }
 });
 
 soundOn?.addEventListener("click", () => {
   soundOn?.classList.add("hide");
   soundOff?.classList.remove("hide");
+  isSoundOn = false;
 
-  if (backgroundMusic) {
-    backgroundMusic.pause();
-  }
+  console.log("Pausing all music");
+  backgroundMusic?.pause();
+  breakMusic?.pause();
 });
+
+if (breakTag) {
+  const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+      if (
+        mutation.type === "attributes" &&
+        mutation.attributeName === "class"
+      ) {
+        console.log("Mutation observed:", mutation);
+        if (isSoundOn) {
+          if (breakTag?.classList.contains("hidden")) {
+            console.log("Observer: Playing background music");
+            backgroundMusic?.play();
+            breakMusic?.pause();
+          } else {
+            console.log("Observer: Playing break music");
+            breakMusic?.play();
+            backgroundMusic?.pause();
+          }
+        }
+      }
+    }
+  });
+
+  observer.observe(breakTag, { attributes: true });
+} else {
+  console.error("breakTag not found");
+}
