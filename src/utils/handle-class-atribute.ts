c704f1061c263fc9d6c8ffed030cbef086d6a295
Bruno Dias <brunodias40@gmail.com>
2024-08-07 06:16:18 -0300

    feat: add utility functions for handling CSS classes

---
+++ src/utils/handle-class-atribute.ts
@@ -0,0 +1,7 @@
+export function addClass(element: Element, className: string) {
+  element?.classList.add(className);
+}
+
+export function removeClass(element: Element, className: string) {
+  element?.classList.remove(className);
+}
